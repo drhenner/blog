421a26a00bbd566ed9bcb78cf272e8cc2af4ade9
David Henner <drhenner@yahoo.com>
2011-04-16 20:48:47 -0700

    add test for sanitizing data

--- spec/models/blog_comment_spec.rb
+++ spec/models/blog_comment_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Blog::BlogComment do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'instance methods' do
+    context 'sanitize_data' do
+      it 'should display unknown' do
+        @post = Factory.build('Blog::BlogComment', :name => nil)
+        @post.save.should be_true
+        @post.name.should == 'Anonymous'
+      end
+    end
+
+  end
 end
